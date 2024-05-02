08029db7a921d38dc07baf9b5ec16aa0aa2b4ae2
vachegunashvili <161934989+vachegunashvili@users.noreply.github.com>
2024-05-02 16:10:41 +0400

    goa

---
+++ qimes,davaleba/davaleba,2/dav.py
@@ -0,0 +1,23 @@
+pepsi=int(input("please eneter products price:"))
+
+fanta=int(input("please eneter products price:"))
+
+fusetea=int(input("pleaase enter products price:"))
+if pepsi>=600 and pepsi<=1000:
+
+    print("im decliling this product,because high price")
+elif pepsi>=1 and pepsi<=600 :
+    print("deal!")
+
+if fanta>=600 and fanta<=1000 :
+    print("im decliling this product,because high price")
+
+elif fusetea>=1 and fusetea<= 600 :
+    print("deal!")
+
+
+if fusetea>=600 and fusetea<= 1000 :
+    print("im decliling this product,because high price")
+elif fusetea>=1 and fusetea<=600:
+
+    print("deal!")
